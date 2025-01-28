d1f40d41e164f1be59b36b75f6cf59a914b61fee
Felipe Macedo Gomes <femacedogomes@gmail.com>
2025-01-28 11:59:27 -0300

    feat: create parceiros page

---
+++ src/app/[locale]/(unauth)/parceiros/page.tsx
@@ -0,0 +1,47 @@
+'use client';
+import type { SanityStoresRespose } from '@/types/sanity';
+import { getStores } from '@/app/services/sanity';
+import { useEffect, useState } from 'react';
+
+const StoresPage = () => {
+  const [stores, setStores] = useState<SanityStoresRespose[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    getStores().then((res) => {
+      setStores(res);
+    }).finally(() => setIsLoading(false));
+  }, []);
+  return (
+    <div>
+      {isLoading
+        ? (
+            <div>Carregando lojas...</div>
+          )
+        : (<h1 className="text-3xl font-bold">Lojas</h1>)}
+      { stores.length === 0 && !isLoading
+        ? (
+            <div>Nenhuma loja encontrada...</div>
+          )
+        : (
+            <div className="flex gap-4">
+              {stores.map(store => (
+                <div key={store._id} className="border-2 border-gray-500 p-4">
+                  <h2>{store.title}</h2>
+                  <p>
+                    {store.productsOrServices?.map(product => (
+                      <div key={product._key}>
+                        <h3>{product.name}</h3>
+                        <p>{product.description}</p>
+                        <p>{product.price}</p>
+                      </div>
+                    ))}
+                  </p>
+                </div>
+              ))}
+            </div>
+          )}
+    </div>
+  );
+};
+export default StoresPage;
