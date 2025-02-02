be6208d34caa18c9337b68650a89ef22ff31a050
Felipe Macedo Gomes <femacedogomes@gmail.com>
2025-02-02 18:29:11 -0300

    feat: improv css for textarea

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -22,7 +22,7 @@ export const Input = ({ label, placeholder, type, isInvalid, value, rows = 5, on
   const baseClasses = `px-3 py-2 text-dark border placeholder-darkLight cursor-pointer ${isInvalid ? 'border-error' : ''}`;
 
   return (
-    <div className="min-w-60 text-dark">
+    <div className="min-w-60">
       {label && <label htmlFor={inputId} className="ml-3 block text-dark text-sm font-normal">{label}</label>}
       {type === 'textArea'
         ? (
@@ -32,7 +32,7 @@ export const Input = ({ label, placeholder, type, isInvalid, value, rows = 5, on
               onChange={onChange}
               placeholder={placeholder}
               rows={rows}
-              className={`${baseClasses} rounded-lg text-dark`}
+              className={`${baseClasses} rounded-xl text-dark`}
               {...(props as TextareaHTMLAttributes<HTMLTextAreaElement>)}
             />
           )
