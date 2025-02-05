dc56aae74b37bfbc862e1e9fb48de3187c33a278
Felipe Macedo Gomes <femacedogomes@gmail.com>
2025-02-04 22:37:32 -0300

    fix: logo mobile sizes

--- src/components/sections/Cta/CtaAgilitySection1.tsx
+++ src/components/sections/Cta/CtaAgilitySection1.tsx
@@ -5,8 +5,8 @@ import Image from 'next/image';
 export const CtaAgility1 = () => {
   return (
     <div className="h-[20vh] bg-grayLight w-full flex gap-6 items-center justify-center">
-      <Image src={Logo} alt="cta-agility2" width={72} height={63} className="max-w-20 h-auto" />
-      <Image src={LogoFull} alt="cta-agility2" width={197} height={56} className="max-w-48 h-auto" />
+      <Image src={Logo} alt="cta-agility2" width={72} height={63} className="max-w-5 sm:max-w-20h-auto" />
+      <Image src={LogoFull} alt="cta-agility2" width={197} height={56} className="max-w-14 sm:max-w-48h-auto" />
     </div>
   );
 };
