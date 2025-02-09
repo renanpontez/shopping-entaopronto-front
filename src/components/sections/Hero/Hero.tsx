947595dc40abf2eccb9ea748d2b2921078ac036a
renan m <renanpontez@gmail.com>
2025-02-09 12:39:58 -0300

    feat: improve hero

---
+++ src/components/sections/Hero/Hero.tsx
@@ -0,0 +1,9 @@
+import Minimal from './HeroMinimal';
+import Primary from './HeroPrimary';
+import Store from './HeroStore';
+
+export const Hero = {
+  Primary,
+  Minimal,
+  Store,
+};
---
+++ src/components/sections/Hero/HeroStore.tsx
@@ -0,0 +1,44 @@
+import Container from '@/components/Container';
+import Typography from '@/components/Typography';
+import Image from 'next/image';
+
+const COMPANY_PLACEHOLDER = '/assets/images/company-placeholder.svg';
+const BANNER_PLACEHOLDER = '/assets/images/bg-mockup.svg';
+
+type Props = {
+  title: string;
+  backgroundUrl?: string;
+  logoUrl?: string;
+  description?: string;
+};
+
+const HeroStore = ({
+  title,
+  backgroundUrl = BANNER_PLACEHOLDER,
+  logoUrl = COMPANY_PLACEHOLDER,
+  // description,
+}: Props) => (
+  <div
+    className="relative flex flex-col justify-center py-10 sm:py-16 w-full text-white text-center overflow-hidden bg-primary bg-cover bg-center"
+    style={{ backgroundImage: `url(${backgroundUrl})` }}
+  >
+    <div className="absolute inset-0 bg-black/60" />
+    <div className="relative z-10">
+      <div className="flex justify-center my-10">
+        <Image src={logoUrl} alt="" aria-hidden="true" width={90} height={90} className="light rounded-xl w-24 h-24" />
+      </div>
+      <Container className="flex flex-col gap-4 pb-1 items-center">
+        <Typography variant="h2">
+          {title}
+        </Typography>
+        {/* TODO: CREATE PORTABLETEXT */}
+        {/* <Typography variant="bodySmall" tag="p" className=" max-w-[400px] text-center">
+           {description}
+        </Typography> */}
+      </Container>
+
+    </div>
+  </div>
+);
+
+export default HeroStore;
