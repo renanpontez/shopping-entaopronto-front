1703773abc2b2c84898e4a23ff4a5eb3b42e3f96
renan m <renanpontez@gmail.com>
2025-10-04 14:50:00 -0300

    feat: improve ctas agility

--- src/components/sections/Cta/CtaAgility.tsx
+++ src/components/sections/Cta/CtaAgility.tsx
@@ -2,7 +2,9 @@ import logoFullWhite from '@/public/assets/images/agility-completo-primary.svg';
 import logoFullPurple from '@/public/assets/images/agility-completo-white.svg';
 import logoWhite from '@/public/assets/images/agility-logo-primary.svg';
 import logoPurple from '@/public/assets/images/agility-logo-white.svg';
+
 import Image from 'next/image';
+import Link from 'next/link';
 
 type CtaAgilityProps = {
   variant?: 'light' | 'purple';
@@ -24,9 +26,11 @@ export const CtaAgility = ({ variant = 'light', classname }: CtaAgilityProps) =>
   };
 
   return (
-    <div className={`py-16 sm:py-20 w-full flex gap-6 items-center justify-center ${variantSelection[variant].backGround} ${classname}`}>
-      <Image src={variantSelection[variant].logo} alt="Agility brand logo" width={72} height={63} className="max-w-5 sm:max-w-20 h-auto" />
-      <Image src={variantSelection[variant].logoFull} alt="Agility full logo" width={197} height={56} className="max-w-14 sm:max-w-32 h-auto" />
-    </div>
+    <Link target="_blank" rel="noopener noreferrer" href="https://agilitycreative.com.br">
+      <div className={`py-16 sm:py-20 w-full flex gap-6 items-center justify-center ${variantSelection[variant].backGround} ${classname}`}>
+        <Image src={variantSelection[variant].logo} alt="Agility brand logo" width={72} height={63} className="max-w-5 sm:max-w-20 h-auto" />
+        <Image src={variantSelection[variant].logoFull} alt="Agility full logo" width={197} height={56} className="max-w-14 sm:max-w-32 h-auto" />
+      </div>
+    </Link>
   );
 };
--- src/components/sections/Hero/HeroPrimary.tsx
+++ src/components/sections/Hero/HeroPrimary.tsx
@@ -4,6 +4,7 @@ import Container from '@/components/Container';
 import Typography from '@/components/Typography';
 import { motion } from 'motion/react';
 import Image from 'next/image';
+import Link from 'next/link';
 
 type Props = {
   title: string;
@@ -48,13 +49,24 @@ const HeroPrimary = ({ title, videoUrl = '/assets/images/test.mp4', imageUrl = '
               animate={{ y: 0, opacity: 1 }}
               transition={{ duration: 0.6, ease: 'easeOut', delay: 0.2 }}
             >
-              <Image
-                src="/assets/images/entao-pronto-logo-white.svg"
-                alt="Logo"
-                width={350}
-                height={100}
-                className="h-16 w-auto"
-              />
+              <div className="flex flex-row gap-6 items-center justify-center">
+                <Link href="#">
+                  <Image
+                    src="/assets/images/entao-pronto-logo-white.svg"
+                    alt="Logo"
+                    width={350}
+                    height={100}
+                    className="h-16 w-auto"
+                  />
+                </Link>
+
+                <Typography variant="h4" tag="p" className="text-white">+</Typography>
+
+                <Link target="_blank" rel="noopener noreferrer" href="https://agilitycreative.com.br" className="py-16 sm:py-20 flex gap-6">
+                  <Image src="/assets/images/agility-logo-white.svg" alt="Agility brand logo" width={72} height={63} className="max-w-5 sm:max-w-15 h-auto" />
+                  <Image src="/assets/images/agility-completo-white.svg" alt="Agility full logo" width={197} height={56} className="max-w-14 sm:max-w-32 h-auto" />
+                </Link>
+              </div>
             </motion.div>
             <motion.div
               initial={{ y: 30, opacity: 0 }}
